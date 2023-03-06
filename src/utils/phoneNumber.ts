e0d9822a9eb32235983bcf254d5000fc879ba6f0
Mango Habanero <main@mangohabanero.dev>
2023-03-06 17:00:40 +0300

    refactor(updates): Better handling of phone Number manipulation.

--- src/utils/phoneNumber.ts
+++ src/utils/phoneNumber.ts
@@ -1,29 +1,32 @@
-import { parsePhoneNumber } from 'libphonenumber-js';
+import { CountryCode, parsePhoneNumber } from 'libphonenumber-js'
 
-/**
- * It takes a phone number as a string, and returns the country code of that phone
- * number as a string
- * @param {string} phoneNumber - The phone number to parse.
- * @returns A promise that resolves to a country code.
- */
-export function getCountryCodeFromPhoneNumber(phoneNumber : string){
-  const formattedNumber = sanitizePhoneNumber(phoneNumber);
-  const parsedPhoneNumber = parsePhoneNumber(formattedNumber);
+
+export function getCountryCodeFromPhoneNumber (phoneNumber: string) {
+  const formattedNumber = sanitizePhoneNumber(phoneNumber)
+  const parsedPhoneNumber = parsePhoneNumber(formattedNumber)
   if (parsedPhoneNumber) {
-    return parsedPhoneNumber.country;
+    return parsedPhoneNumber.country
   } else {
-    throw new Error(`Could not retrieve country code phone number: ${phoneNumber}`);
+    throw new Error(
+      `Could not retrieve country code phone number: ${phoneNumber}`
+    )
   }
 }
 
-export function sanitizePhoneNumber(phoneNumber : string) {
+export function sanitizePhoneNumber (
+  phoneNumber: string,
+  countryCode?: CountryCode
+) {
   // Remove any trailing whitespace
-  phoneNumber = phoneNumber.trim();
+  phoneNumber = phoneNumber.trim()
 
-  // TODO[Philip]: Consider improving this in the future. POC scope is limited to CM, KE and ZA for now, so it's fine.
-  // if phone number doesn't start with a +, add it
   if (!phoneNumber.startsWith('+')) {
-    phoneNumber = `+${phoneNumber}`
+    try {
+      const parsedPhoneNumber = parsePhoneNumber(phoneNumber, countryCode)
+      phoneNumber = parsedPhoneNumber.number
+    } catch (err) {
+      throw new Error(`Could not parse phone number: ${phoneNumber}`)
+    }
   }
-  return phoneNumber;
+  return phoneNumber
 }
