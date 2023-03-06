74a17f9aa7dcf3f39a3b49b2da4db6ea88994f4d
Mango Habanero <main@mangohabanero.dev>
2023-03-06 16:47:50 +0300

    chore(improve translations): Namespaces translations.

---
+++ src/lib/i18n/en/helpers/index.ts
@@ -0,0 +1,11 @@
+import type { NamespaceHelpersTranslation } from "../../i18n-types";
+
+const en_helpers = {
+  emptyLanguageSet:
+    'No more languages options.',
+	emptyVoucherSet:
+    'No more vouchers.'
+
+} satisfies NamespaceHelpersTranslation
+
+export default en_helpers
---
+++ src/lib/i18n/sw/helpers/index.ts
@@ -0,0 +1,9 @@
+import { BaseTranslation } from "../../i18n-types";
+
+const sw_helpers: BaseTranslation = {
+  emptyLanguageSet:
+    'Hamna lugha nyingine.',
+  emptyVoucherSet:
+    'Hamna Sarafu nyingine.'
+}
+export default sw_helpers
