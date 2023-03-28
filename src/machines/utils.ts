31e8f9f144b12d791bf57942813d292e16cd6e3b
Mango Habanero <main@mangohabanero.dev>
2023-03-28 09:20:38 +0300

    refactor(machines and translations): Adds util functions for dried up machine interactions.

--- src/machines/utils.ts
+++ src/machines/utils.ts
@@ -1,23 +1,102 @@
-import {PostgresDb} from "@fastify/postgres";
-import {GraphQLClient} from "graphql-request";
-import {Provider} from "ethers";
-import {Redis as RedisClient} from "ioredis";
-import {CountryCode} from "libphonenumber-js";
-import {ActiveVoucher} from "@lib/ussd/voucher";
-import {Account} from "@db/models/account";
-import {Session} from "@lib/ussd/session";
-import {supportedLanguages} from "@lib/ussd/utils";
-import {GraphUser} from "@lib/graph/user";
-import {tHelpers} from "@src/i18n/translator";
+import { PostgresDb } from '@fastify/postgres';
+import { GraphQLClient } from 'graphql-request';
+import { Provider } from 'ethers';
+import { Redis as RedisClient } from 'ioredis';
+import { CountryCode } from 'libphonenumber-js';
+import { ActiveVoucher } from '@lib/ussd/voucher';
+import { Account } from '@db/models/account';
+import { Session } from '@lib/ussd/session';
+import { supportedLanguages } from '@lib/ussd/utils';
+import { Gender, GraphUser } from '@lib/graph/user';
+import { tHelpers } from '@src/i18n/translator';
+import { sanitizePhoneNumber } from '@utils/phoneNumber';
+import { MachineError } from '@lib/errors';
+import { Cache } from '@utils/redis';
+import { Transaction } from '@machines/statement';
+
+enum BaseError {
+  INVALID_PHONE_NUMBER = "INVALID_PHONE_NUMBER",
+  SELF_INTERACTION = "SELF_INTERACTION",
+  UNKNOWN_ACCOUNT = "UNKNOWN_ACCOUNT",
+}
+
+export enum MachineId {
+  AUTH = "auth",
+  BALANCES = "balances",
+  LANGUAGES = "languages",
+  MAIN = "main",
+  PINS = "pins",
+  PROFILE = "profile",
+  REGISTRATION = "registration",
+  SETTINGS = "settings",
+  SOCIAL_RECOVERY = "socialRecovery",
+  STATEMENT = "statement",
+  TRANSFER = "transfer",
+  VOUCHER = "voucher",
+}
+
+interface ContextData {
+  communityBalance?: string,
+  guardians?: {
+    entry?: {
+      toAdd?: string,
+      toRemove?: string
+    },
+    loaded?: string[],
+    validated?: {
+      toAdd?: string,
+      toRemove?: string
+    }
+  },
+  languages?: {
+    selected?: string
+  },
+  personal_information?: {
+    given_names?: string,
+    family_name?: string,
+    gender?: Gender,
+    year_of_birth?: number,
+    location_name?: string,
+  },
+  pins?: {
+    initial?: string,
+    wards?: {
+      entry?: string,
+      validated?: string
+    }
+  },
+  statement?: string[],
+  transfer?: {
+    amount?: number,
+    recipient?: {
+      entry?: string,
+      tag?: string
+      validated?: string
+    }
+  },
+  vouchers?: {
+    balances?: string[],
+    held?: string[],
+    selected?: string
+    info?: {
+      contact?: string;
+      description?: string;
+      location?: string;
+      name?: string;
+      symbol?: string;
+    }
+  }
+}
 
 export interface BaseContext {
-  data?: Record<string, any>,
+  data?: ContextData
   errorMessages?: string[],
   resources?: Resources,
   session?: Session,
   user?: User,
   ussd?: Ussd
 }
+
 export interface Resources {
   db: PostgresDb
   e_redis: RedisClient
@@ -29,9 +108,14 @@ export interface Resources {
 
 export interface User {
   account?: Account,
-  activeVoucher: ActiveVoucher,
   graph?: Partial<GraphUser>
-  transactionTag?: string
+  guardians?: string[]
+  tag?: string
+  transactions?: Transaction[]
+  vouchers?: {
+    active?: ActiveVoucher
+    held?: ActiveVoucher[]
+  }
 }
 
 export interface Ussd {
@@ -58,6 +142,7 @@ export function clearErrorMessages (context: BaseContext, event: any) {
 export function isOption (expected: string, input: string) {
   return input === expected
 }
+
 function generateOptionChecker(expectedValue: string) {
   return function (context: BaseContext, event: any) {
     return isOption(expectedValue, event.input);
@@ -76,6 +161,15 @@ export const isOption00 = generateOptionChecker('00');
 export const isOption11 = generateOptionChecker('11');
 export const isOption22 = generateOptionChecker('22');
 
+export function isValidPhoneNumber(context: BaseContext, event: any) {
+  const { ussd: { countryCode } } = context;
+  return validatePhoneNumber(countryCode, event.input)[0] === '+';
+}
+
+export function isSuccess(context: BaseContext, event: any) {
+  return event.data.success
+}
+
 export async function languageOptions () {
   const languagesList = Object.values(supportedLanguages)
     .filter((obj) => Object.keys(obj)[0] !== 'fb')
@@ -84,18 +178,21 @@ export async function languageOptions () {
   return await menuPages(languagesList, placeholder)
 }
 
-export async function menuPages (list: string[], placeholder: string) {
-  const pages = []
+export async function menuPages(list: string[], placeholder: string): Promise<string[][]> {
+  const pages = [];
   for (let i = 0; i < list.length; i += 3) {
-    pages.push(list.slice(i, i + 3))
+    pages.push(list.slice(i, i + 3));
   }
-  return pages.map(group => {
-    if (group.length === 0){
-      return placeholder
+  while (pages.length < 3) {
+    pages.push([]);
+  }
+  return pages.map((group) => {
+    if (group.length === 0) {
+      return placeholder;
     } else {
-      return group.join('\n')
+      return group.join("\n");
     }
-  })
+  });
 }
 
 export async function translate(state: string, translator:any, data?: Record<string, any>){
@@ -105,13 +202,39 @@ export async function translate(state: string, translator:any, data?: Record<str
     return translator[state]()
   }
 }
+
 export function updateErrorMessages (context: BaseContext, event: any) {
   const errorMessages = context.errorMessages || []
   const { message } = event.data
-  console.error(`State machine error occurred: ${JSON.stringify(message)}.`)
+  console.error(`State machine error occurred: ${JSON.stringify(message)}. STACKTRACE: ${event.data.stack}`)
   errorMessages.push(message)
   context.errorMessages = errorMessages
   return context
 }
 
+export function validatePhoneNumber(countryCode: CountryCode, phoneNumber: string, ) {
+  try {
+    return sanitizePhoneNumber(phoneNumber, countryCode);
+  } catch (error) {
+    throw new MachineError(BaseError.INVALID_PHONE_NUMBER, error.message)
+  }
+}
 
+export async function validateUser(countryCode: CountryCode, phoneNumber: string, redis: RedisClient): Promise<User> {
+  const key = validatePhoneNumber(countryCode, phoneNumber)
+  const cache = new Cache(redis, key)
+  const user = await cache.getJSON<User>()
+  if (!user) {
+    throw new MachineError(BaseError.UNKNOWN_ACCOUNT, `Account not found for: ${key}`)
+  }
+  return user
+}
+
+export async function validateTargetUser(context: BaseContext, input: string) {
+  const { user: { account: { phone_number } }, resources: { p_redis }, ussd: { countryCode } } = context
+  const targetUser = await validateUser(countryCode, input, p_redis)
+  if (targetUser.account.phone_number === phone_number) {
+    throw new MachineError(BaseError.SELF_INTERACTION, "Cannot interact with self.")
+  }
+  return targetUser
+}
