e70386bf7ca3145e4bee3f6f9cbf52d2a54e843b
Mango Habanero <main@mangohabanero.dev>
2023-04-30 10:04:43 +0300

    refactor(db): Improves data storage.
    - Switches to model centric interactions with db.
    - [accounts]
      - Renames "RESETTING_PASSWORD" to "RESETTING_PIN".
      - Adds "active_voucher_address" column to "accounts" table.
    - [sessions]
      - Adds session type field.
      - Eases null checks on "machine_state", "machines" and "responses" columns.
     - [guardians]
       - Switches to one to many "account_phone_number" to "guardian" pattern.

--- src/db/models/guardian.ts
+++ src/db/models/guardian.ts
@@ -1,55 +1,66 @@
 import { PostgresDb } from '@fastify/postgres';
-import { Redis as RedisClient } from 'ioredis';
-import { Cache } from '@utils/redis';
-import { User } from '@machines/utils';
+import { logger } from '@/app';
 
-export async function addGuardian(db: PostgresDb, guardian: string, redis: RedisClient, ward: string){
-  const client = await db.connect();
-  const cache = new Cache<User>(redis, ward)
-  try {
-    await Promise.all([
-      client.query(
-        `INSERT INTO guardians (account_phone_number, wards)
-         VALUES ($1, ARRAY[$2])
-         ON CONFLICT (account_phone_number) DO UPDATE SET wards = array_append(guardians.wards, $2::text)
-         RETURNING *`,
-        [guardian, ward]),
-      cache.getJSON()
-        .then(user => {
-          const guardians = user?.guardians || [];
-          if (!guardians.includes(guardian)) {
-            guardians.push(guardian);
-            return cache.updateJSON({guardians});
-          }
-        })
-    ]);
-  } finally {
-    client.release();
-  }
+interface GuardianInterface {
+  account_phone_number: string
+  guardian: string
 }
 
-export async function removeGuardian(db: PostgresDb, guardian: string, redis: RedisClient, ward: string){
-  const client = await db.connect();
+export class Guardian {
 
-  try {
-    await client.query(
-      `UPDATE guardians
-       SET wards = array_remove(guardians.wards, $2)
-       WHERE account_phone_number = $1
-       RETURNING *`,
-      [guardian, ward]
-    );
-  } finally {
-    client.release();
+  constructor(private db: PostgresDb) {}
+
+  public async deleteGuardian(guardian: string, phoneNumber: string) {
+    const client = await this.db.connect()
+    try {
+      await client.query(
+        `DELETE FROM guardians WHERE account_phone_number = $1 AND guardian = $2`,
+        [phoneNumber, guardian]
+      )
+    } catch (error) {
+      client.release()
+      logger.error(`Error deleting guardian: ${error}`)
+    }
   }
 
-  const cache = new Cache<User>(redis, ward);
-  const user = await cache.getJSON();
-  const guardians = user?.guardians || [];
+  public async insertGuardian(guardian: string, phoneNumber: string) {
+    const client = await this.db.connect()
+    try {
+        await client.query(
+          `INSERT INTO guardians (account_phone_number, guardian)
+            VALUES ($1, $2)`,
+          [phoneNumber, guardian]
+        )
+    } catch (error) {
+      client.release()
+      logger.error(`Error inserting guardian: ${error}`)
+    }
+  }
 
-  const index = guardians.indexOf(guardian);
-  if (index !== -1) {
-    guardians.splice(index, 1);
-    await cache.updateJSON({ guardians });
+  public async selectGuardian(guardian: string, phoneNumber: string) {
+    const client = await this.db.connect()
+    try {
+      const { rows } = await client.query(
+        `SELECT guardian FROM guardians WHERE account_phone_number = $1 AND guardian = $2`,
+        [phoneNumber, guardian]
+      )
+      console.log(rows)
+      return rows.length > 0 ? rows[0].guardian : null;
+    } finally {
+      client.release()
+    }
+  }
+
+  public async selectAllGuardians(phoneNumber: string): Promise<string[]> {
+    const client = await this.db.connect()
+    try {
+      const { rows } = await client.query<GuardianInterface>(
+        `SELECT guardian FROM guardians WHERE account_phone_number = $1`,
+        [phoneNumber]
+      )
+      return rows.map((row: GuardianInterface) => row.guardian)
+    } finally {
+      client.release()
+    }
   }
 }
