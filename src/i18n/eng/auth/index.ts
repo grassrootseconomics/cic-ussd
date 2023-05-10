57fcbc8c89ccdf2af8210deb9088e1bdb1be90d1
Mango Habanero <main@mangohabanero.dev>
2023-05-10 13:34:06 +0300

    refactor(menus): Updates initial PIN prompts
    Closes #46

--- src/i18n/eng/auth/index.ts
+++ src/i18n/eng/auth/index.ts
@@ -6,9 +6,9 @@ const eng_auth: NamespaceAuthTranslation = {
     processingAccount:
         "END Your account is still being created. You will receive an SMS when your account is ready.",
     enteringPin:
-        "CON Please enter four number PIN:\n00. Exit",
+        "CON Please enter a new four number PIN for your account:\n00. Exit",
     confirmingPin:
-        "CON Enter your PIN again:\n00. Exit",
+        "CON Enter your four number PIN again:\n00. Exit",
     mainMenu:
         "CON Balance: {balance|currency} {symbol}\n1. Send\n2. My Vouchers\n3. My Account\n4. Help",
     activationError:
