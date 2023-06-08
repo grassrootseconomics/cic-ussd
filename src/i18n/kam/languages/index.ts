8474860cfa5942f5c380897d99400a239ee8f858
mango-habanero <main@mango-habanero.dev>
2023-06-08 10:18:41 +0300

    fix(translations): Fixes translation for word "enter" in enteringPin state.
    Closes #85

--- src/i18n/kam/languages/index.ts
+++ src/i18n/kam/languages/index.ts
@@ -9,7 +9,7 @@ const kam_languages: NamespaceLanguagesTranslation = {
     'CON Thina waumila ivinda ya kusakua luka, tata ingi kavindane kangi.',
   changeSuccess:
     'CON Woni waku wa kusakua luka niwatumwa.\n0. Syoka\n9. Uma',
-  enteringPin: 'CON Ikera PIN yaku:\n0. Syoka',
+  enteringPin: 'CON Ikia PIN yaku:\n0. Syoka',
   exit: exit,
   firstLanguageSet: 'CON Sakua luka:\n{languages}\n0. Syoka\n00. Uma\n11. Mbee',
   secondLanguageSet: 'CON Sakua luka:\n{languages}\n11. Mbee\n22. Syoka',
